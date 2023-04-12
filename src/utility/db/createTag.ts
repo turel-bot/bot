b964157fa7c6566cd28daf20a31146abe4236268
AceLikesGhosts <acelikesghosts@gmail.com>
2023-04-11 17:02:51 -0700

    a lot

---
+++ src/utility/db/createTag.ts
@@ -0,0 +1,23 @@
+import type { Tag } from '@prisma/client';
+import { prismaClient } from '../../index';
+import type OKType from '../OKType';
+import getTag from './getTag';
+import { v4 } from 'uuid';
+
+async function createTag(name: string, guild: string, content: string): Promise<OKType<Tag> | null>
+{
+    const findTag: OKType<Tag> = await getTag(name, guild);
+    if(!findTag.ok || findTag === null) return null;
+
+    const newTag = await prismaClient.tag.create({
+        data: {
+            id: v4(),
+            content: content,
+            name: name,
+            guild: guild
+        }
+    });
+
+}
+
+export default createTag;
