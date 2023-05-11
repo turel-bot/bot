91a88ca684e378a140aacf340ce362218ff5aaf2
AceLikesGhosts <acelikesghosts@gmail.com>
2023-05-11 00:07:54 -0700

    feat: status storing pull last on ready

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,6 +1,6 @@
 import type TClient from '../structures/TClient';
 import Event from '../structures/Event';
-import { ActivityType } from 'discord.js';
+import { prismaClient } from '../index';
 
 class ready extends Event
 {
@@ -12,7 +12,15 @@ class ready extends Event
     public async execute(client: TClient): Promise<void>
     {
         console.log(`Logged in on account ${ client.user?.tag } on ${ new Date() }`);
-        client.user?.setActivity('on a Razer Gaming Phone', { type: ActivityType.Streaming, url: 'https://twitch.tv/turelbot' });
+        // client.user?.setActivity('on a Razer Gaming Phone', { type: ActivityType.Streaming, url: 'https://twitch.tv/turelbot' });
+        
+        const status = await prismaClient.status.findFirst({
+            where: {
+                shard: 0
+            }
+        });
+
+        client.user?.setActivity(status!.text, { type: status?.type, url: status?.type === 1 ? status.url ?? 'https://twitch.tv/turelbot' : '' });
     }
 }
 
