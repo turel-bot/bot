970808e53f392a14b574749722cb4b4374bfe2ff
AceLikesGhosts <acelikesghosts@gmail.com>
2023-07-06 15:33:40 -0700

    chore: update install script

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,56 +1,53 @@
 'use strict';
 
+/** @private @constant */
+var green = '\x1b[32m';
+/** @private @constant */
+var red = '\x1b[0;31m';
+/** @private @constant */
+var reset = '\x1b[0m';
+/** @private @constant */
+var exec = require('child_process').exec;
 
-// self running function
-new function ()
-{
+/**
+ * @param {string} c 
+ * @param {string} t 
+ * @returns {string}
+ */
+function color(c, t) {
+    return c + t + reset;
+}
+
+/**
+ * @param {string} d 
+ * @returns { Promise<{ stdout: string, stderr: string }> }
+ */
+async function cmd(d) {
+    return new Promise((re, rej) => {
+        exec(d, ((err, stdout, stderr) => {
+            if(err) {
+                rej(err);
+            } else {
+                re({ stdout, stderr })
+            }
+        }));
+    });
+}
+
+async function runCmd(d,m) {
+    cmd(d)
+    .then(() => console.log(`${color(green, '*')} ${m}`))
+    .catch(console.error);
+}
+
+async function start() {
     var start = Date.now();
-    // colors
-    var green = '\x1b[32m';
-    var reset = '\x1b[0m';
+    await runCmd('cat .env.example > .env', 'Copied default configuration to `.env`.'); // friendly way to `mv` that doesnt restrict to bash, powershell can run this too! :D
+    await runCmd('npm ci --quiet --no-progress', 'Finished installing packages.');
+    await runCmd('npx prisma generate', 'Finished generating Prisma files.');
+    await runCmd('npx tsc', 'Finished transpiling Typescript to Javascript.');
+    console.log(`${color(green, '*')} Finished setting up in ${Date.now() - start}ms.`);
+}
 
-    var { exec } = require('child_process');
-    Promise.all([
-        exec('npm install --quiet --no-progress', (function (e)
-        {
-            if (e && e instanceof Error)
-            {
-                console.error('Failed to run installation script, weird.\r\n' + e);
-                return;
-            }
-            console.log(green + '*' + reset + ' Finished installing NPM packages.');
-        })),
-
-        exec('npx prisma generate', (function (e)
-        {
-            if (e && e instanceof Error)
-            {
-                console.error('Failed to run Prisma Generate, weird.\r\n' + e);
-                return;
-            }
-            console.log(green + '*' + reset + ' Finished running Prisma generate.');
-        })),
-
-        exec('npx tsc', (function (e)
-        {
-            if (e && e instanceof Error)
-            {
-                console.error('Failed to run TSC, weird.\r\n' + e);
-                return;
-            }
-            console.log(green + '*' + reset + ' Finished transpiling Typescript into Javascript.');
-        })),
-
-        exec('cat .env.example > .env', (function (e)
-        {
-            if (e && e instanceof Error)
-            {
-                console.error('Failed to run installation script, weird.\r\n' + e);
-                return;
-            }
-            console.log(green + '*' + reset + ' Copied default configuration to `.env`.');
-        }))
-    ])
-
-    console.log(green + '*' + reset + ' Finished installation script, took ' + Date.now() - start + ' ms.');
-}();
+start()
+    .catch((e) => e);
