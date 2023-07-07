732eb06473004eacd1f587b6f46a85b6d58f882a
AceLikesGhosts <acelikesghosts@gmail.com>
2023-07-06 22:43:26 -0700

    chore!: command system pt 1/1000

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -1,23 +1,12 @@
 /* eslint-disable */
-import { CustomError } from '@biased-ts/eor';
 import type { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js';
 
 /**
  * @description Represents a DiscordJS <code>Command</code>.
  */
-class Command
-{
-    public readonly data: SlashCommandBuilder;
-    public constructor(data: SlashCommandBuilder)
-    {
-        this.data = data;
-    }
-
-    // @ts-expect-error
-    public async execute(interaction: ChatInputCommandInteraction): Promise<any>
-    {
-        CustomError.createAndThrow('NotImplemented', 'Function #execute was not implemented by a class which extends command.');
-    }
+interface Command {
+    readonly data: SlashCommandBuilder;
+    execute(interaction: ChatInputCommandInteraction): Promise<any>
 }
 
 export default Command;
